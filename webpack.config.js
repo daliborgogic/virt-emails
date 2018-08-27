c31aedf9b405349b1a1055b8c48692a24b967790
dlbr <mail@daliborgogic.com>
2018-08-27 14:23:56 +0200

    conditionally inject

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const { filters } = require('./src/filters')
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        use: [
-          'html-loader',
-          {
-            loader: 'pug-plain-loader',
-            options: { filters }
-          }
-        ]
-      },
-      {
-        test: /\.styl$/,
-        use: [
-          'css-loader',
-          'stylus-loader'
-        ]
-      }
+module.exports = (env, argv) => {
+  const isDev =  argv.mode === 'development'
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          use: [
+            'html-loader',
+            {
+              loader: 'pug-plain-loader',
+              options: { filters }
+            }
+          ]
+        },
+        {
+          test: /\.styl$/,
+          use: [
+            'css-loader',
+            'stylus-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.pug',
+        filename: './index.html',
+        inject: isDev
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.pug',
-      filename: './index.html',
-      inject: true
-    })
-  ]
+  }
 }
